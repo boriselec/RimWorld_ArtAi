ba389d77694318c26a4fc841bc92d17e0fd034a0
boriselec <boris.puzikov@gmail.com>
2023-02-23 15:16:59 +0300

    in progress server description

--- Source/ArtAi14/ArtAi/ITab_Art_Generated.cs
+++ Source/ArtAi14/ArtAi/ITab_Art_Generated.cs
@@ -66,7 +66,7 @@ namespace ArtAi
                     break;
                 case GenerationStatus.InProgress:
                     Text.Font = GameFont.Small;
-                    Widgets.Label(rect3, "Loading...");
+                    Widgets.Label(rect3, image.Description ?? "Loading...");
                     break;
                 case GenerationStatus.Error:
                 default:
